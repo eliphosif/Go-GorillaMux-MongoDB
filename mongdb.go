3e178d3cf926b072485f7d539f8c7a6b761bdd1c
eliphosif <eliphosifhardy@gmail.com>
2022-01-29 23:01:25 +0530

    third commit

--- mongdb.go
+++ mongdb.go
@@ -3,11 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+
 	_ "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	_ "go.mongodb.org/mongo-driver/mongo/readpref"
-	"log"
 )
 
 var AllCustomers *mongo.Collection
@@ -36,70 +37,3 @@ func insertCustomerDoc(AllCustomers *mongo.Collection, cust Customer, ctx contex
 	fmt.Println(res)
 	return res, nil
 }
-
-/*
-	defer func() {
-		if errDB = client.Disconnect(ctx); errDB != nil {
-			panic(errDB)
-		}
-	}()
-
-
-	podcastcollection := quickStartDB.Collection("podcast")
-	episodescollection := quickStartDB.Collection("episode")
-
-	podcastResult, err := podcastcollection.InsertOne(ctx, bson.D{
-		{Key: "title", Value: "this istest tile"},
-		{Key: "suthoer", Value: "Nic niove"},
-		{Key: "tags", Value: bson.A{"devel", "priag", "docu"}},
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(podcastResult.InsertedID)
-
-	episodeResult, err := episodescollection.InsertMany(ctx, []interface{}{
-		bson.D{
-			{Key: "podcast", Value: podcastResult.InsertedID},
-			{Key: "episode", Value: "episode_1"},
-			{Key: "duration", Value: 32},
-		},
-		bson.D{
-			{Key: "podcast", Value: podcastResult.InsertedID},
-			{Key: "episode", Value: "episode_2"},
-			{Key: "duration", Value: 22},
-		},
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(episodeResult.InsertedIDs)
-
-
-
-	//filter documents
-		opts := options.Find()
-	opts.SetSort(bson.D{{"age", -1}})
-	filterCursor, err := AllCustomers.Find(ctx, bson.D{{"firstname", bson.D{{"$in", bson.A{"Alice", "Bob"}}}}}, opts)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var eachCustomer []bson.M
-
-	if err := filterCursor.All(ctx, &eachCustomer); err != nil {
-		log.Fatal(err)
-	}
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer filterCursor.Close(ctx)
-	for filterCursor.Next(ctx) {
-		var eachCustomer bson.M
-		if err = filterCursor.Decode(&eachCustomer); err != nil {
-			log.Fatal(err)
-		}
-	}
-*/
--- routes.go
+++ routes.go
@@ -1,15 +1,19 @@
 package main
 
-import (  
+import (
 	"fmt"
+	"io"
 	"log"
-	"net/http" 
-	"github.com/gorilla/mux"  
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 )
+
 func initlizeRouter() {
 
 	r := mux.NewRouter()
-
+	r.HandleFunc("/", welcome).Methods("GET")
 	r.HandleFunc("/api/login", agentLogin).Methods("GET")
 	r.HandleFunc("/api/customers", getCustomers).Methods("GET")
 	r.HandleFunc("/api/customer/{id}", getCustomer).Methods("GET")
@@ -18,6 +22,14 @@ func initlizeRouter() {
 	r.HandleFunc("/api/customer/{id}", deleteCustomers).Methods("DELETE")
 
 	fmt.Println("server is listening")
-	log.Fatal(http.ListenAndServe(":8090", r))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 
+}
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "welcome to my app")
 }
